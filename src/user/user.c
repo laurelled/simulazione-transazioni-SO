8e1efa95820d597ffeae04717f7ba93b41f5b467
Light <bfn.lorenzo@gmail.com>
2022-07-28 19:16:04 +0200

    update user.c

--- src/user/user.c
+++ src/user/user.c
@@ -3,15 +3,17 @@
 #include "../transaction.h"
 #include "../utils/utils.h"
 
-static int SO_BUDGET_INIT = 0;
-static int SO_USERS_NUM = 0;
-static int SO_NODES_NUM = 0;
-static int SO_REWARD = 0;
-static int SO_MIN_TRANS_GEN_NSEC = 0;
-static int SO_MAX_TRANS_GEN_NSEC = 0;
+//Variabili statici per ogni processo utente
+static int SO_BUDGET_INIT;
+static int SO_USERS_NUM;
+static int SO_NODES_NUM;
+static int SO_REWARD;
+static int SO_MIN_TRANS_GEN_NSEC;
+static int SO_MAX_TRANS_GEN_NSEC;
 
 transaction generate_transaction(int* utenti_da_cui_scegliere, int max_importo, int* nodi_da_cui_scegliere)
 {
+  int bilancio_corrente = 0;
   SO_BUDGET_INIT = retrieve_constant("SO_BUDGET_INIT");
   SO_USERS_NUM = retrieve_constant("SO_USERS_NUM");
   SO_NODES_NUM = retrieve_constant("SO_NODES_NUM");
@@ -20,34 +22,36 @@ transaction generate_transaction(int* utenti_da_cui_scegliere, int max_importo, 
   SO_MAX_TRANS_GEN_NSEC = retrieve_constant("SO_MAX_TRANS_GEN_NSEC");
 
 
-  int bilancio_corrente = calcola_bilancio();
-  while (bilancio_corrente = calcola_bilancio() >= 2)
+  while ((bilancio_corrente = calcola_bilancio()) >= 2)
   {
     transaction t;
     // estrazione di un destinatario casuale
     int random_user = (rand() % (SO_USERS_NUM + 1));
-    while (getpid() == utenti_da_cui_scegliere[random_user])
-    { // evita di estrarre lo stesso processo in cui ci troviamo
+    while (getpid() == utenti_da_cui_scegliere[random_user])//TODO trovare un utente non terminato
+    { // evita di estrarre lo stesso processo in cui ci troviamo o di trovare un utente terminato
       random_user = (rand() % (SO_USERS_NUM + 1));
     }
     // estrazione di un nodo casuale
     int random_node = (rand() % (SO_NODES_NUM + 1));
     // generazione di un importo da inviare 
     int num = (rand() % (bilancio_corrente - 2 + 1)) + 2;
-    int cifra_nodo = num / 100 * SO_REWARD;
-    if (cifra_nodo == 0)
-      cifra_nodo = 1;
-    int cifra_utente = bilancio_corrente - cifra_nodo;
+    int reward_t = num / 100 * SO_REWARD;
+    if (reward_t == 0)
+      reward_t = 1;
+    int cifra_utente = num - reward_t;
 
     // creazione di una transazione e invio di tale al nodo generato
-    new_transaction(&t, getpid(), utenti_da_cui_scegliere[random_user], cifra_utente, cifra_nodo);
+    new_transaction(&t, getpid(), utenti_da_cui_scegliere[random_user], cifra_utente, reward_t);
     //TODO invio della transazione al nodo
 
 
     // tempo di attesa dopo l'invio di una transazione
-    long maxT = retrieve_constant("SO_MAX_TRANS_GEN_NSEC");
-    long minT = retrieve_constant("SO_MIN_TRANS_GEN_NSEC");
-    sleep(rand() % (maxT - minT + 1) + minT);
+    //block sigprocmask
+
+    sleep_random_from_range(SO_MIN_TRANS_GEN_NSEC, SO_MAX_TRANS_GEN_NSEC);
+
+    //unblock sigprocmask
+    //kill(getppid(), SIGUSR1);
   }
 }
 
