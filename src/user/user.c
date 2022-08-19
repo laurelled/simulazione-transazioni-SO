dd8576a4253c83fcf6cc4a497efe7bb378599826
Lorenzo Bafunno <lorenzo.bafunno@unito.it>
2022-08-19 15:39:40 +0200

    aggiunto SIGSEGV ai segnali gestiti e aggiunto un SHM_RDONLY per evitare scritture accidentali

--- src/user/user.c
+++ src/user/user.c
@@ -22,7 +22,6 @@
 /*Variabili statici per ogni processo utente*/
 extern int SO_BUDGET_INIT;
 extern int SO_USERS_NUM;
-extern int SO_NODES_NUM;
 extern int SO_REWARD;
 extern int SO_RETRY;
 extern int SO_MIN_TRANS_GEN_NSEC;
@@ -43,6 +42,7 @@ void init_user(int* users, int shm_nodes_array, int shm_nodes_size, int shm_book
   struct sembuf sops;
   int bilancio_corrente = SO_BUDGET_INIT;
   int block_reached = 0;
+  int i = 0;
   struct sigaction sa;
   int queue_id;
   sigset_t mask;
@@ -65,25 +65,29 @@ void init_user(int* users, int shm_nodes_array, int shm_nodes_size, int shm_book
     fprintf(ERR_FILE, "user u%d: cannot associate handler to SIGTERM\n", getpid());
     exit(EXIT_FAILURE);
   }
+  if (sigaction(SIGSEGV, &sa, NULL) < 0) {
+    fprintf(ERR_FILE, "user u%d: could not associate handler to SIGSEGV.\n", getpid());
+    exit(EXIT_FAILURE);
+  }
 
   if ((sem_id = semget(getppid(), 0, S_IRUSR | S_IWUSR)) == -1) {
     fprintf(ERR_FILE, "user u%d: err\n", getpid());
     exit(EXIT_FAILURE);
   }
 
-  if ((nodes.array = attach_shm_memory(shm_nodes_array)) == NULL) {
+  if ((nodes.array = attach_shm_memory(shm_nodes_array, SHM_RDONLY)) == NULL) {
     fprintf(ERR_FILE, "user u%d: the process cannot be attached to the nodes array shared memory.\n", getpid());
     exit(EXIT_FAILURE);
   }
-  if ((nodes.size = attach_shm_memory(shm_nodes_size)) == NULL) {
+  if ((nodes.size = attach_shm_memory(shm_nodes_size, SHM_RDONLY)) == NULL) {
     fprintf(ERR_FILE, "user u%d: the process cannot be attached to the nodes size shared memory.\n", getpid());
     exit(EXIT_FAILURE);
   }
-  if ((book.blocks = attach_shm_memory(shm_book_id)) == NULL) {
+  if ((book.blocks = attach_shm_memory(shm_book_id, SHM_RDONLY)) == NULL) {
     fprintf(ERR_FILE, "user u%d: the process cannot be attached to the registry shared memory.\n", getpid());
     exit(EXIT_FAILURE);
   }
-  if ((book.size = attach_shm_memory(shm_book_size_id)) == NULL) {
+  if ((book.size = attach_shm_memory(shm_book_size_id, SHM_RDONLY)) == NULL) {
     fprintf(ERR_FILE, "user u%d: the process cannot be attached to the book size shared memory.\n", getpid());
     exit(EXIT_FAILURE);
   }
@@ -91,25 +95,26 @@ void init_user(int* users, int shm_nodes_array, int shm_nodes_size, int shm_book
   while (cont_try < SO_RETRY)
   {
     if ((bilancio_corrente = calcola_bilancio(bilancio_corrente, book, &block_reached)) >= 2) {
+      int size = 0;
       transaction t;
       /* estrazione di un destinatario casuale*/
-      int random_user = random_element(users, SO_USERS_NUM);
       /* estrazione di un nodo casuale*/
-      int random_node = random_element(nodes.array, *nodes.size);
-      int cifra_utente;
-      int num = 0;
-      int reward;
+      int cifra_utente, random_user, random_node, num, reward;
       struct msg* message = malloc(sizeof(struct msg));
       if (message == NULL) {
         TEST_ERROR;
         CHILD_STOP_SIMULATION;
         exit(EXIT_FAILURE);
       }
+      fprintf(ERR_FILE, "u%d: bilancio corrente=%d\n", getpid(), bilancio_corrente);
+      random_user = random_element(users, SO_USERS_NUM);
       if (random_user == -1) {
         fprintf(LOG_FILE, "init_user u%d:  all other users have terminated. Ending successfully.\n", getpid());
         exit(EXIT_SUCCESS);
       }
-
+      size = *(nodes.size);
+      i++;
+      random_node = random_element(nodes.array, size);
       if (random_node == -1) {
         fprintf(ERR_FILE, "init_user u%d: all nodes have terminated, cannot send transaction.\n", getpid());
         exit(EXIT_FAILURE);
@@ -159,6 +164,7 @@ void init_user(int* users, int shm_nodes_array, int shm_nodes_size, int shm_book
     /*tempo di attesa dopo l'invio di una transazione*/
     sleep_random_from_range(SO_MIN_TRANS_GEN_NSEC, SO_MAX_TRANS_GEN_NSEC);
   }
+  fprintf(ERR_FILE, "u%d: esco\n", getpid());
   exit(EARLY_FAILURE);
 }
 
@@ -166,8 +172,8 @@ int calcola_bilancio(int bilancio, struct master_book book, int* block_reached)
 {
   /* block_reached e book.size codificati come indici di blocchi (come se fosse un'array di blocchi)
     necessaria conversione in indice per l'array nella shm (array di transazioni) */
-  int i = *block_reached * SO_BLOCK_SIZE;
-  int size = *book.size * SO_BLOCK_SIZE;
+  int i = (*block_reached) * SO_BLOCK_SIZE;
+  int size = (*book.size) * SO_BLOCK_SIZE;
   while (i < size) {
     transaction t = book.blocks[i];
     if (t.receiver == getpid()) {
@@ -195,6 +201,10 @@ void usr_handler(int signal) {
   case SIGTERM:
     /* fprintf(LOG_FILE, "u%d: killed by parent. Ending successfully\n", getpid()); */
     exit(EXIT_SUCCESS);
+    break;
+  case SIGSEGV:
+    fprintf(ERR_FILE, "u%d: recieved a SIGSEGV, stopping simulation.\n", getpid());
+    exit(EXIT_FAILURE);
     break;
   default:
     fprintf(ERR_FILE, "user %d: an unexpected signal was recieved.\n", getpid());
