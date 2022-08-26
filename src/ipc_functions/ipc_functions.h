0bdd5d498e50639bc8517726e75c2dec7a1ced7d
Lorenzo Bafunno <lorenzo.bafunno@unito.it>
2022-08-27 00:23:09 +0200

    add IPC_CREATION_FLAGS

--- src/ipc_functions/ipc_functions.h
+++ src/ipc_functions/ipc_functions.h
@@ -4,6 +4,8 @@
 #define ID_MEM 1
 #define ID_READY_ALL 0
 
+#define IPC_CREATION_FLAGS IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR
+
 int sem_reserve(int sem_id, int sem_num);
 int sem_release(int sem_id, int sem_num);
 int sem_wait_for_zero(int sem_id, int sem_num);
