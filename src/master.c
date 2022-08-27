f58fb1b8597bb9fd8070506166a3fccd25040dcf
Lorenzo Bafunno <lorenzo.bafunno@unito.it>
2022-08-27 14:11:33 +0200

    move master.c

--- src/master.c
+++ src/master.c
@@ -1,9 +1,9 @@
-#include "../utils/utils.h"
-#include "../load_constants/load_constants.h"
-#include "../ipc/ipc.h"
-#include "../master_book/master_book.h"
-#include "../node/node.h"
-#include "../user/user.h"
+#include "./utils/utils.h"
+#include "./load_constants/load_constants.h"
+#include "./ipc/ipc.h"
+#include "./master_book/master_book.h"
+#include "./master_utils/master_utils.h"
+#include "./user/user.h"
 
 #include <sys/ipc.h>
 #include <sys/sem.h>
@@ -12,16 +12,11 @@
 #include <sys/stat.h>
 #include <sys/msg.h>
 
-#define SIM_END_SEC 0
-#define SIM_END_USR 1
-#define SIM_END_SIZ 2
-
-#define MAX_USERS_TO_PRINT 20
-
 #define ID_READY_NODE 2
 #define NUM_SEM 3
 
 #define REGISTRY_SIZE sizeof(transaction) * SO_BLOCK_SIZE * SO_REGISTRY_SIZE
+
 
 extern int SO_USERS_NUM;
 extern int SO_NODES_NUM;
@@ -36,15 +31,11 @@ static int shm_book_size_id = -1;
 static struct master_book book;
 
 static int shm_users_array_id = -1;
-static int* users = NULL;
-static int* user_budget;
-static int inactive_users;
+static struct users_ds users;
 
 static int shm_nodes_array_id = -1;
 static int shm_nodes_size_id = -1;
-static int* nodes_write_fd;
-static int* node_budget;
-static struct nodes_list nodes;
+static struct nodes_ds nodes;
 
 static int queue_id = -1;
 static int refused_queue_id = -1;
@@ -53,50 +44,8 @@ static int sem_id = -1;
 
 static int periodical_flag;
 
-int periodical_update(int block_reached) {
-  int index;
-  transaction t;
-  int size = *(book.size) * SO_BLOCK_SIZE;
-
-  while (block_reached < size) {
-    t = book.blocks[block_reached];
-    if (t.sender == SELF_SENDER) {
-      int nodes_size = *(nodes.size);
-      int index = find_element(nodes.array, nodes_size, t.receiver);
-      node_budget[index] += t.quantita;
-    }
-    else {
-      int total_quantity = t.quantita + t.reward;
-      index = find_element(users, SO_USERS_NUM, t.sender);
-      user_budget[index] -= total_quantity;
-      index = find_element(users, SO_USERS_NUM, t.receiver);
-      user_budget[index] += t.quantita;
-    }
-    ++block_reached;
-  }
-
-  return block_reached;
-}
-
-void stop_simulation() {
-  int size;
-  int i = 0;
-  int child = 0;
-
-  while ((child = users[i]) != 0 && i < SO_USERS_NUM) {
-    i++;
-    kill(child, SIGTERM);
-  }
-  i = 0;
-  size = *(nodes.size);
-  while ((child = nodes.array[i]) != 0 && i < size) {
-    i++;
-    kill(child, SIGTERM);
-  }
-
-  /* resetting errno as some kill might have failed */
-  errno = 0;
-}
+static void wait_siblings(int sem_num);
+static int* assign_friends(int* array, int size);
 
 static void cleanup() {
   sigset_t mask;
@@ -106,8 +55,8 @@ static void cleanup() {
   sig_block(sblock, 3);
 
 
-  if (users != NULL && nodes.array != NULL) {
-    stop_simulation();
+  if (users.array != NULL && nodes.array != NULL) {
+    stop_simulation(users.array, nodes);
   }
   errno = 0;
   if (shm_book_id != -1) {
@@ -143,127 +92,19 @@ static void cleanup() {
     TEST_ERROR;
   }
 
-  while (nodes_write_fd[i] != 0) {
-    close(nodes_write_fd[i]);
+  while (nodes.write_pipes[i] != 0) {
+    close(nodes.write_pipes[i]);
     TEST_ERROR;
     i++;
   }
 
-  free(node_budget);
-  free(user_budget);
-  free(nodes_write_fd);
+  free(nodes.budgets);
+  free(nodes.write_pipes);
+  free(users.budgets);
 }
 
-void periodical_print() {
-  int i = 0;
-  printf("NUMBER OF ACTIVE USERS %d | NUMBER OF ACTIVE NODES %d\n", (SO_USERS_NUM - inactive_users), *(nodes.size));
-  /* budget di ogni processo utente (inclusi quelli terminati prematuramente)*/
-  printf("USERS (%d) BUDGETS\n", SO_USERS_NUM);
-  if (SO_USERS_NUM <= MAX_USERS_TO_PRINT) {
-    while (i < SO_USERS_NUM) {
-      printf("USER u%d : %d$\n", users[i], user_budget[i]);
-      i++;
-    }
-  }
-  else {
-    int min_i = 0, max_i = 0;
-    printf("[!] Users count is too high to display all budgets [!]\n");
-    i = 1;
-    while (i < SO_NODES_NUM) {
-      if (user_budget[i] < user_budget[min_i]) {
-        min_i = i;
-      }
-      if (user_budget[i] > user_budget[max_i]) {
-        max_i = i;
-      }
-      i++;
-    }
 
-    printf("HIGHEST BUDGET: USER u%d : %d$\n", users[max_i], user_budget[max_i]);
-    printf("LOWEST BUDGET: USER u%d : %d$\n", users[min_i], user_budget[min_i]);
-  }
-
-  {
-    int size = *(nodes.size);
-    int i = 0;
-    /* budget di ogni processo nodo */
-    printf("NODES (%d) BUDGETS\n", *(nodes.size));
-    while (i < size) {
-      printf("NODE n%d : %d$\n", nodes.array[i], node_budget[i]);
-      i++;
-    }
-  }
-}
-
-void summary_print(int ending_reason, int* users, int* user_budget, struct nodes_list nodes, int* node_budget, int* nof_transactions) {
-  int i = 0;
-
-  switch (ending_reason)
-  {
-  case SIM_END_SEC:
-    printf("[!] Simulation ending reason: TIME LIMIT REACHED [!]\n\n");
-    break;
-  case SIM_END_SIZ:
-    printf("[!] Simulation ending reason: MASTER BOOK SIZE EXCEEDED [!]\n\n");
-    break;
-  case SIM_END_USR:
-    printf("[!] Simulation ending reason: ALL USERS TERMINATED [!]\n\n");
-    break;
-  default:
-    printf("[!] Simulation ending reason: UNEXPECTED ERRORS [!]\n\n");
-    break;
-  }
-  /* bilancio di ogni processo utente, compresi quelli che sono terminati prematuramente */
-  /*printf("USERS BUDGETS\n");
-  while (i < SO_USERS_NUM) {
-    printf("USER u%d : %d$\n", users[i], user_budget[i]);
-    i++;
-  }*/
-  /* bilancio di ogni processo nodo */
-  i = 0;
-  printf("NODES (%d) BUDGETS\n", *(nodes.size));
-  while (i < *nodes.size) {
-    printf("NODE n%d : %d$\n", nodes.array[i], node_budget[i]);
-    i++;
-  }
-  printf("NUMBER OF INACTIVE USERS: %d\n", inactive_users);
-  printf("NUMBER OF TRANSACTION BLOCK WRITTEN INTO THE MASTER BOOK: %d\n\n\n", *book.size);
-
-  printf("Number of transactions left per node:\n");
-  i = 0;
-  while (i < *nodes.size) {
-    printf("NODE %d: %d transactions left\n", nodes.array[i], nof_transactions[i]);
-    i++;
-  }
-}
-
-void wait_siblings(int sem_num) {
-  sem_reserve(sem_id, sem_num);
-  sem_wait_for_zero(sem_id, sem_num);
-}
-
-int* assign_friends(int* array, int size) {
-  int i = 0;
-  int* friends = init_list(MAX_NODES - 1);
-  if (friends == NULL)
-    return NULL;
-  while (i < SO_NUM_FRIENDS) {
-    int random_el = random_element(array, size);
-    if (random_el == -1) {
-      fprintf(stderr, "%s:%d: something went wrong with the extraction of friends\n", __FILE__, __LINE__);
-      free(friends);
-      return NULL;
-    }
-    /* se trova un pid già inserito, estrae un altro pid */
-    if (find_element(friends, SO_NUM_FRIENDS, random_el) != -1)
-      continue;
-    friends[i] = random_el;
-    i++;
-  }
-  return friends;
-}
-
-void handler(int signal) {
+static void sig_handler(int signal) {
   switch (signal) {
   case SIGALRM:
     /*alarm per la stampa periodica*/
@@ -313,7 +154,7 @@ void handler(int signal) {
       incoming_t = message.mtext;
 
       /* refusing transaction if nodes array is full */
-      if (*(nodes.size) == MAX_NODES) {
+      if (*(nodes.size_ptr) == MAX_NODES) {
         int sender = incoming_t.sender;
         if (kill(sender, 0) != -1) {
           int refuse_q = msgget(getpid() - 1, 0);
@@ -344,13 +185,13 @@ void handler(int signal) {
         TEST_ERROR_AND_FAIL;
 
         {
-          struct nodes_list nodes;
+          bzero(&nodes, sizeof(struct nodes_ds));
           nodes.array = shmat(shm_nodes_array_id, NULL, SHM_RDONLY);
           TEST_ERROR_AND_FAIL;
-          nodes.size = shmat(shm_nodes_size_id, NULL, SHM_RDONLY);
+          nodes.size_ptr = shmat(shm_nodes_size_id, NULL, SHM_RDONLY);
           TEST_ERROR_AND_FAIL;
 
-          friends = assign_friends(nodes.array, *(nodes.size));
+          friends = assign_friends(nodes.array, *(nodes.size_ptr));
           if (friends == NULL) {
             fprintf(stderr, "n%d: something went wrong while trying to create friends list\n" __FILE__, __LINE__);
             CHILD_STOP_SIMULATION;
@@ -358,7 +199,7 @@ void handler(int signal) {
           }
 
           shmdt(nodes.array);
-          shmdt(nodes.size);
+          shmdt(nodes.size_ptr);
         }
 
         /* resetting signal mask to override the parent's */
@@ -389,7 +230,7 @@ void handler(int signal) {
 
         /* sending through pipe the new generated node pid to SO_NUM_FRIENDS node */
         while (i < SO_NUM_FRIENDS) {
-          int node_random = random_element(nodes.array, *(nodes.size));
+          int node_random = random_element(nodes.array, *(nodes.size_ptr));
           if (node_random == -1) {
             fprintf(stderr, "%s:%d: something went wrong with the extraction of the node\n", __FILE__, __LINE__);
             cleanup();
@@ -397,9 +238,9 @@ void handler(int signal) {
           }
           /* search if the current node_random wasn't already extracted */
           if (find_element(extracted_nodes, SO_NUM_FRIENDS, node_random) == -1) {
-            int index = find_element(nodes.array, *(nodes.size), node_random);
+            int index = find_element(nodes.array, *(nodes.size_ptr), node_random);
             extracted_nodes[i] = node_random; /* add node_random to the list to avoid duplicates */
-            write(nodes_write_fd[index], &child, sizeof(int));
+            write(nodes.write_pipes[index], &child, sizeof(int));
             TEST_ERROR_AND_FAIL;
             kill(node_random, SIGUSR2);
             ++i;
@@ -407,7 +248,7 @@ void handler(int signal) {
         }
         free(extracted_nodes);
         /* write the write end of the pipe in the same index of the node pid in nodes.array */
-        nodes_write_fd[*(nodes.size)] = file_descriptors[1];
+        nodes.write_pipes[*(nodes.size_ptr)] = file_descriptors[1];
 
         new_node_msg_id = msgget(child, 0);
         TEST_ERROR_AND_FAIL;
@@ -416,8 +257,8 @@ void handler(int signal) {
         TEST_ERROR_AND_FAIL;
         kill(child, SIGUSR1);
 
-        nodes.array[*(nodes.size)] = child;
-        (*nodes.size)++;
+        nodes.array[*(nodes.size_ptr)] = child;
+        (*nodes.size_ptr)++;
       }
     }
   }
@@ -430,14 +271,14 @@ int main() {
 
   load_constants();
 
-  nodes_write_fd = init_list(MAX_NODES);
+  nodes.write_pipes = init_list(MAX_NODES);
   TEST_ERROR_AND_FAIL;
-  node_budget = init_list(MAX_NODES);
+  nodes.budgets = init_list(MAX_NODES);
   TEST_ERROR_AND_FAIL;
-  user_budget = init_list(SO_USERS_NUM);
+  users.budgets = init_list(SO_USERS_NUM);
   TEST_ERROR_AND_FAIL;
   while (i < SO_USERS_NUM) {
-    user_budget[i] = SO_BUDGET_INIT;
+    users.budgets[i] = SO_BUDGET_INIT;
     i++;
   }
   printf("[!] MASTER PID: %d\n", getpid());
@@ -467,15 +308,15 @@ int main() {
   /* Inizializzazione shm users array */
   shm_users_array_id = shmget(IPC_PRIVATE, sizeof(int) * SO_USERS_NUM, IPC_CREATION_FLAGS);
   TEST_ERROR_AND_FAIL;
-  users = shmat(shm_users_array_id, NULL, 0);
+  users.array = shmat(shm_users_array_id, NULL, 0);
   TEST_ERROR_AND_FAIL;
 
   /* Inizializzazione shm per size dei nodi */
   shm_nodes_size_id = shmget(IPC_PRIVATE, sizeof(int), IPC_CREATION_FLAGS);
   TEST_ERROR_AND_FAIL;
-  nodes.size = shmat(shm_nodes_size_id, NULL, 0);
+  nodes.size_ptr = shmat(shm_nodes_size_id, NULL, 0);
   TEST_ERROR_AND_FAIL;
-  *(nodes.size) = SO_NODES_NUM;
+  *(nodes.size_ptr) = SO_NODES_NUM;
 
   /* Inizializzazione shm nodi array */
   shm_nodes_array_id = shmget(IPC_PRIVATE, sizeof(int) * MAX_NODES, IPC_CREATION_FLAGS);
@@ -495,7 +336,7 @@ int main() {
     struct sigaction act;
     bzero(&mask, sizeof(sigset_t));
     sigemptyset(&mask);
-    act.sa_handler = handler;
+    act.sa_handler = sig_handler;
     act.sa_flags = 0;
     act.sa_mask = mask;
     sigaction(SIGALRM, &act, NULL);
@@ -554,7 +395,7 @@ int main() {
     default:
       close(fd[0]);
       TEST_ERROR_AND_FAIL;
-      nodes_write_fd[i] = fd[1];
+      nodes.write_pipes[i] = fd[1];
       nodes.array[i] = child;
       i++;
       break;
@@ -575,14 +416,13 @@ int main() {
       exit(EXIT_FAILURE);
     case 0:
     {
-      int* users = shmat(shm_users_array_id, NULL, SHM_RDONLY);
       TEST_ERROR_AND_FAIL;
       wait_siblings(ID_SEM_READY_ALL);
-      init_user(users, shm_nodes_array_id, shm_nodes_size_id, shm_book_id, shm_book_size_id);
+      init_user(shm_users_array_id, shm_nodes_array_id, shm_nodes_size_id, shm_book_id, shm_book_size_id);
       break;
     }
     default:
-      users[i] = child;
+      users.array[i] = child;
       i++;
       break;
     }
@@ -594,19 +434,20 @@ int main() {
 
   {
     int ending_reason, block_reached = 0;
-    int* nof_transactions = init_list(MAX_NODES);
+
+    nodes.transactions_left = init_list(MAX_NODES);
     TEST_ERROR_AND_FAIL;
 
     alarm(1);
     i = 0;
-    while (simulation_seconds < SO_SIM_SEC && inactive_users < SO_USERS_NUM && *book.size < SO_REGISTRY_SIZE) {
+    while (simulation_seconds < SO_SIM_SEC && users.inactive_count < SO_USERS_NUM && *book.size < SO_REGISTRY_SIZE) {
       int status;
       int terminated_p;
 
       if (periodical_flag) {
         periodical_flag = 0;
-        block_reached = periodical_update(block_reached);
-        periodical_print();
+        block_reached = periodical_update(block_reached, users, nodes, book);
+        periodical_print(users, nodes);
       }
 
       terminated_p = wait(&status);
@@ -620,12 +461,11 @@ int main() {
       if (WIFEXITED(status)) {
         if (WEXITSTATUS(status) == EXIT_FAILURE) {
           fprintf(stderr, "Child %d encountered an error. Stopping simulation\n", terminated_p);
-          free(nof_transactions);
           cleanup();
           exit(EXIT_FAILURE);
         }
         else {
-          int size = *(nodes.size);
+          int size = *(nodes.size_ptr);
           int node_index = 0;
           if ((node_index = find_element(nodes.array, size, terminated_p)) != -1) {
             fprintf(stderr, "master: node %d terminated with the unexpected status %d while sim was ongoing. Check the code.\n", terminated_p, WEXITSTATUS(status));
@@ -633,7 +473,7 @@ int main() {
             exit(EXIT_FAILURE);
           }
           else {
-            inactive_users++;
+            users.inactive_count++;
           }
         }
       }
@@ -644,7 +484,7 @@ int main() {
       int sblock[2] = { SIGUSR1, SIGUSR2 };
       sig_block(sblock, 2);
     }
-    stop_simulation();
+    stop_simulation(users.array, nodes);
     printf("\n\nSIMULATION ENDED IN %dS\n", simulation_seconds);
 
     /* wait all of the terminated child */
@@ -653,9 +493,9 @@ int main() {
       int status;
       while ((terminated_p = wait(&status)) != -1) {
         int index;
-        if ((index = find_element(nodes.array, *(nodes.size), terminated_p)) != -1) {
+        if ((index = find_element(nodes.array, *(nodes.size_ptr), terminated_p)) != -1) {
           if (WIFEXITED(status)) {
-            nof_transactions[index] = WEXITSTATUS(status);
+            nodes.transactions_left[index] = WEXITSTATUS(status);
           }
         }
       }
@@ -668,22 +508,48 @@ int main() {
     if (simulation_seconds == SO_SIM_SEC) {
       ending_reason = SIM_END_SEC;
     }
-    else if (inactive_users == SO_USERS_NUM) {
+    else if (users.inactive_count == SO_USERS_NUM) {
       ending_reason = SIM_END_USR;
     }
     else if (*book.size == SO_REGISTRY_SIZE) {
       ending_reason = SIM_END_SIZ;
     }
 
-    periodical_update(block_reached);
-    summary_print(ending_reason, users, user_budget, nodes, node_budget, nof_transactions);
-    free(nof_transactions);
+    periodical_update(block_reached, users, nodes, book);
+    summary_print(ending_reason, users, nodes, *(book.size));
+    free(nodes.transactions_left);
   }
 
-  users = NULL;
+  users.array = NULL;
   nodes.array = NULL;
   cleanup();
 
   printf("Completed simulation. Exiting...\n");
   exit(EXIT_SUCCESS);
+}
+
+static void wait_siblings(int sem_num) {
+  sem_reserve(sem_id, sem_num);
+  sem_wait_for_zero(sem_id, sem_num);
+}
+
+static int* assign_friends(int* array, int size) {
+  int i = 0;
+  int* friends = init_list(MAX_NODES - 1);
+  if (friends == NULL)
+    return NULL;
+  while (i < SO_NUM_FRIENDS) {
+    int random_el = random_element(array, size);
+    if (random_el == -1) {
+      fprintf(stderr, "%s:%d: something went wrong with the extraction of friends\n", __FILE__, __LINE__);
+      free(friends);
+      return NULL;
+    }
+    /* se trova un pid già inserito, estrae un altro pid */
+    if (find_element(friends, SO_NUM_FRIENDS, random_el) != -1)
+      continue;
+    friends[i] = random_el;
+    i++;
+  }
+  return friends;
 }
